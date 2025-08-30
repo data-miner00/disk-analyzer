39c8e29e68cc4a4c6d0c7d8e7bc72724d4cc6c2f
Shaun Chong <data-miner00@users.noreply.github.com>
2025-08-30 14:33:21 +0800

    initial

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,32 @@
+use sysinfo::{
+    Disks,
+    System,
+};
+
+#[tauri::command]
+fn get_disks() -> Vec<String> {
+    let mut sys = System::new_all();
+
+    sys.refresh_all();
+
+    Disks::new_with_refreshed_list()
+        .iter()
+        .map(|disk| format!("{}: {} bytes", disk.name().to_string_lossy(), disk.total_space()))
+        .collect()
+}
+
+// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_opener::init())
+        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![get_disks])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
