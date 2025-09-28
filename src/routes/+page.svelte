81e47686fca3cb18036161bb5cd0efa8711cb98f
Shaun Chong <data-miner00@users.noreply.github.com>
2025-09-28 10:01:03 +0800

    refactor: Remove frontend chart

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,29 +2,10 @@
   import { invoke } from "@tauri-apps/api/core";
   import { HardDrive } from "@lucide/svelte";
   import { onMount } from "svelte";
-  import { LineChart } from "layerchart";
-  import TrendingUpIcon from "@lucide/svelte/icons/trending-up";
-  import { scaleUtc } from "d3-scale";
-  import { curveNatural } from "d3-shape";
-  import * as Chart from "$lib/components/ui/chart/index.js";
-  import * as Card from "$lib/components/ui/card/index.js";
   import { type Disk } from "$lib/types";
-
-  type ChartConfig = {
-    key: string;
-    label: string;
-    color: string;
-  };
 
   let diskInfo = $state<Disk[]>([]);
   let isLoading = $state(true);
-  let disksHistory = $state<DiskDto[]>([]);
-  let containerConfig = $state<Chart.ChartConfig>(
-    {} satisfies Chart.ChartConfig
-  );
-  let chartConfig = $state<ChartConfig[]>([]);
-  let aggregatedUsedData = $state<any[]>([]);
-  let aggregatedUsedPctData = $state<any[]>([]);
 
   type DiskDto = {
     id: number;
@@ -39,7 +20,9 @@
   }
 
   async function process_daily_disk_info() {
-    disksHistory = (await invoke("read_disk_dtos", { count: 20 })) as DiskDto[];
+    const disksHistory = (await invoke("read_disk_dtos", {
+      count: 20,
+    })) as DiskDto[];
     const lastEntry = disksHistory[disksHistory.length - 1];
     let newIndex = lastEntry ? lastEntry.id + 1 : 1;
     const lastRecordedDate = lastEntry ? new Date(lastEntry.date) : null;
@@ -71,7 +54,6 @@
   onMount(async () => {
     await get_disks();
     await process_daily_disk_info();
-    aggregateDiskHistoryData();
     isLoading = false;
   });
 
@@ -79,71 +61,8 @@
     return (bytes / 1024 ** 3).toFixed(2) + " GB";
   }
 
-  function toGBNumber(bytes: number): number {
-    return parseFloat((bytes / 1024 ** 3).toFixed(2));
-  }
-
   function toPercentage(used: number, total: number): number {
     return (used / total) * 100;
-  }
-
-  function aggregateDiskHistoryData() {
-    let index = 1;
-    const distinctDisks = new Set(disksHistory.map((x) => x.name));
-    Array.from(distinctDisks).forEach((diskName) => {
-      const diskEntries = disksHistory.filter(
-        (entry) => entry.name === diskName
-      );
-      const idDiskName = diskName.toLowerCase().replace(/\s+/g, "_");
-      const days = new Set(diskEntries.map((entry) => entry.date));
-
-      days.forEach((day) => {
-        const entryForDay = diskEntries.find((entry) => entry.date === day);
-
-        const currentUsedAgg = aggregatedUsedData.find(
-          (entry) => entry.date.toISOString().split("T")[0] === day
-        );
-        const aggregatedUsedEntry: any = currentUsedAgg || {
-          date: new Date(day),
-        };
-
-        const currentUsedPctAgg = aggregatedUsedPctData.find(
-          (entry) => entry.date.toISOString().split("T")[0] === day
-        );
-        const aggregatedUsedPctEntry: any = currentUsedPctAgg || {
-          date: new Date(day),
-        };
-
-        if (entryForDay) {
-          const diskInfoEntry = diskInfo.find((disk) =>
-            diskName.startsWith(disk.name)
-          );
-          aggregatedUsedEntry[idDiskName] = toGBNumber(
-            diskInfoEntry!.total_space - entryForDay.available_space
-          );
-          aggregatedUsedPctEntry[idDiskName] = toPercentage(
-            diskInfoEntry!.total_space - entryForDay.available_space,
-            diskInfoEntry!.total_space
-          );
-        }
-
-        if (!currentUsedAgg) {
-          aggregatedUsedData.push(aggregatedUsedEntry);
-          aggregatedUsedPctData.push(aggregatedUsedPctEntry);
-        }
-      });
-
-      containerConfig[idDiskName] = {
-        label: diskName,
-        color: `var(--chart-${index})`,
-      };
-
-      chartConfig.push({
-        key: idDiskName,
-        label: diskName,
-        color: `var(--chart-${index++})`,
-      });
-    });
   }
 </script>
 
@@ -185,137 +104,6 @@
     {/each}
   </ul>
 </main>
-
-<Card.Root>
-  <Card.Header>
-    <Card.Title>Disk Usage</Card.Title>
-    <Card.Description
-      >Showing a breakdown of individual disk usages for the current captured PC</Card.Description
-    >
-  </Card.Header>
-  <Card.Content class="pl-12">
-    <Chart.Container config={containerConfig}>
-      <LineChart
-        data={aggregatedUsedData}
-        x="date"
-        xScale={scaleUtc()}
-        series={chartConfig}
-        props={{
-          spline: { curve: curveNatural, motion: "tween", strokeWidth: 2 },
-          xAxis: {
-            format: (v: Date) =>
-              v.toLocaleDateString("en-US", { day: "2-digit" }),
-          },
-          yAxis: {
-            format: (v: number) => `${v} GB`,
-          },
-          highlight: { points: { r: 4 } },
-        }}
-      >
-        {#snippet tooltip()}
-          <Chart.Tooltip label="Used Space">
-            {#snippet formatter({ name, value, item })}
-              <div
-                class="w-3 h-3 rounded"
-                style={`background: ${item.color}`}
-              ></div>
-              <div
-                class="text-muted-foreground flex min-w-[130px] items-center text-xs"
-              >
-                {name.substring(0, 15)}
-                <div
-                  class="text-foreground ml-auto flex items-baseline gap-0.5 font-mono font-medium tabular-nums"
-                >
-                  {value}
-                  <span class="text-muted-foreground font-normal"> GB </span>
-                </div>
-              </div>
-            {/snippet}
-          </Chart.Tooltip>
-        {/snippet}
-      </LineChart>
-    </Chart.Container>
-  </Card.Content>
-  <Card.Footer>
-    <div class="flex w-full items-start gap-2 text-sm">
-      <div class="grid gap-2">
-        <div class="flex items-center gap-2 font-medium leading-none">
-          Trending up by 5.2% this month <TrendingUpIcon class="size-4" />
-        </div>
-        <div class="text-muted-foreground flex items-center gap-2 leading-none">
-          January - June 2024
-        </div>
-      </div>
-    </div>
-  </Card.Footer>
-</Card.Root>
-
-<div class="py-6"></div>
-
-<Card.Root>
-  <Card.Header>
-    <Card.Title>Disk Usage in Percentage</Card.Title>
-    <Card.Description
-      >Showing a breakdown of individual disk usages in percentage for the
-      current captured PC</Card.Description
-    >
-  </Card.Header>
-  <Card.Content class="pl-12">
-    <Chart.Container config={containerConfig}>
-      <LineChart
-        data={aggregatedUsedPctData}
-        x="date"
-        xScale={scaleUtc()}
-        series={chartConfig}
-        props={{
-          spline: { curve: curveNatural, motion: "tween", strokeWidth: 2 },
-          xAxis: {
-            format: (v: Date) =>
-              v.toLocaleDateString("en-US", { day: "2-digit" }),
-          },
-          yAxis: {
-            format: (v: number) => `${v}%`,
-          },
-          highlight: { points: { r: 4 } },
-        }}
-      >
-        {#snippet tooltip()}
-          <Chart.Tooltip label="Usage Percentage">
-            {#snippet formatter({ name, value, item })}
-              <div
-                class="w-3 h-3 rounded"
-                style={`background: ${item.color}`}
-              ></div>
-              <div
-                class="text-muted-foreground flex min-w-[130px] items-center text-xs"
-              >
-                {name.substring(0, 15)}
-                <div
-                  class="text-foreground ml-auto flex items-baseline gap-0.5 font-mono font-medium tabular-nums"
-                >
-                  {(value as number).toFixed(2)}
-                  <span class="text-muted-foreground font-normal"> % </span>
-                </div>
-              </div>
-            {/snippet}
-          </Chart.Tooltip>
-        {/snippet}
-      </LineChart>
-    </Chart.Container>
-  </Card.Content>
-  <Card.Footer>
-    <div class="flex w-full items-start gap-2 text-sm">
-      <div class="grid gap-2">
-        <div class="flex items-center gap-2 font-medium leading-none">
-          Trending up by 5.2% this month <TrendingUpIcon class="size-4" />
-        </div>
-        <div class="text-muted-foreground flex items-center gap-2 leading-none">
-          January - June 2024
-        </div>
-      </div>
-    </div>
-  </Card.Footer>
-</Card.Root>
 
 <style lang="postcss">
   @reference "tailwindcss";
