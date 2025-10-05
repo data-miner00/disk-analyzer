560a20b1aedb7ba91c7e92fe0286063d5d4155ca
Shaun Chong <data-miner00@users.noreply.github.com>
2025-10-05 19:06:31 +0800

    feat: Add selection setting UI

---
+++ src/lib/components/custom/setting-select.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+  import * as Select from "$lib/components/ui/select/index.js";
+
+  type Selectable = {
+    value: string;
+    label: string;
+  };
+
+  type Props = {
+    title: string;
+    description: string;
+    defaultLabel: string;
+    options: Selectable[];
+    initialValue: string;
+    onChange: (newValue: string) => void;
+    // Add icon
+  };
+
+  let props: Props = $props();
+  let value = $state(props.initialValue);
+  const triggerContent = $derived(
+    props.options.find((f) => f.value === value)?.label ?? props.defaultLabel
+  );
+</script>
+
+<div
+  class="flex items-center py-4 justify-between px-6 rounded-lg border border-solid border-gray-200"
+>
+  <div>
+    <p class="font-semibold">{props.title}</p>
+    <p>{props.description}</p>
+  </div>
+  <div>
+    <Select.Root
+      type="single"
+      name="favoriteFruit"
+      bind:value
+      onValueChange={props.onChange}
+    >
+      <Select.Trigger class="w-[180px]">
+        {triggerContent}
+      </Select.Trigger>
+      <Select.Content>
+        <Select.Group>
+          <!-- <Select.Label>Fruits</Select.Label> -->
+          {#each props.options as option (option.value)}
+            <Select.Item
+              value={option.value}
+              label={option.label}
+              disabled={option.value === "grapes"}
+            >
+              {option.label}
+            </Select.Item>
+          {/each}
+        </Select.Group>
+      </Select.Content>
+    </Select.Root>
+  </div>
+</div>
--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -1,16 +1,27 @@
 <script lang="ts">
   import { invoke } from "@tauri-apps/api/core";
   import SettingSwitch from "$lib/components/custom/setting-switch.svelte";
+  import SettingSelect from "$lib/components/custom/setting-select.svelte";
+
+  type ByteFormat = {
+    value: "b" | "kb" | "mb" | "gb" | "tb";
+    label: string;
+    description: string;
+  };
 
   type Settings = {
     darkMode: boolean;
     searchBar: boolean;
+    language: string;
+    byteFormat: string;
   };
 
-  const settings: Settings = {
+  const settings: Settings = $state({
     darkMode: false,
     searchBar: false,
-  };
+    language: "en",
+    byteFormat: "gb",
+  });
 
   function updateDarkMode(newValue: boolean): void {
     settings.darkMode = newValue;
@@ -19,6 +30,35 @@
   function updateSearchBar(newValue: boolean): void {
     settings.searchBar = newValue;
   }
+
+  function updateLanguage(newValue: string): void {
+    settings.language = newValue;
+  }
+
+  function updateByteFormat(newValue: string): void {
+    settings.byteFormat = newValue;
+  }
+
+  type Language = {
+    value: string;
+    label: string;
+  };
+
+  const availableLanguages: Language[] = [
+    { value: "en", label: "English" },
+    { value: "ko", label: "한국어" },
+    { value: "ja", label: "日本語" },
+    { value: "ms", label: "Bahasa Melayu" },
+    { value: "pt", label: "Português" },
+  ];
+
+  const availableByteFormats: ByteFormat[] = [
+    { value: "b", label: "B", description: "Bytes" },
+    { value: "kb", label: "KB", description: "Kilobytes" },
+    { value: "mb", label: "MB", description: "Megabytes" },
+    { value: "gb", label: "GB", description: "Gigabytes" },
+    { value: "tb", label: "TB", description: "Terabytes" },
+  ];
 </script>
 
 <main>
@@ -39,6 +79,28 @@
       description="Whether to enable the search bar functionality for applicable pages."
       initialValue={settings.searchBar}
       onChange={updateSearchBar}
+    />
+  </div>
+
+  <div class="my-4">
+    <SettingSelect
+      title="Language"
+      description="The language used for the user interface from a limited preset."
+      defaultLabel="Select language"
+      options={availableLanguages}
+      initialValue={settings.language}
+      onChange={updateLanguage}
+    />
+  </div>
+
+  <div class="my-4">
+    <SettingSelect
+      title="Byte Display"
+      description="The format for which the bytes will be displayed by default."
+      defaultLabel="Select byte format"
+      options={availableByteFormats}
+      initialValue={settings.byteFormat}
+      onChange={updateByteFormat}
     />
   </div>
 </main>
