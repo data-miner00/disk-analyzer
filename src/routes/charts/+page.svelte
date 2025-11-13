0d2a408a379009cda6f3a2adabce8593ec0d002a
Shaun Chong <data-miner00@users.noreply.github.com>
2025-11-13 18:17:41 +0800

    feat: Remove old usage change chart

--- src/routes/charts/+page.svelte
+++ src/routes/charts/+page.svelte
@@ -1,164 +1,22 @@
 <script lang="ts">
   import * as Card from "$lib/components/ui/card";
   import * as Chart from "$lib/components/ui/chart";
-  import { type DiskDto, type ChartConfig } from "$lib/types";
   import { TrendingUpIcon } from "@lucide/svelte";
-  import { invoke } from "@tauri-apps/api/core";
   import { scaleUtc } from "d3-scale";
   import { curveNatural } from "d3-shape";
   import { LineChart } from "layerchart";
-  import { onMount } from "svelte";
 
   const { data } = $props();
-  const availableSpaceReport = data.availableSpaceReport;
-  const usedSpaceReport = data.usedSpaceReport;
-  const usedSpacePctReport = data.usedSpacePctReport;
-  const usedSpaceChgPctReport = data.usedSpaceChgPctReport;
-
-  let disksHistory = $state<DiskDto[]>([]);
-  let aggregatedData = $state<any[]>([]);
-  let containerConfig = $state<Chart.ChartConfig>(
-    {} satisfies Chart.ChartConfig
-  );
-  let chartConfig = $state<ChartConfig[]>([]);
-  onMount(async () => {
-    disksHistory = (await invoke("read_disk_dtos", { count: 20 })) as DiskDto[];
-    calculateUsageDiff();
-  });
-
-  type DiskDtoWithDiff = DiskDto & { usage_diff: number };
-
-  function calculateUsageDiff() {
-    let index = 1;
-    const distinctDisks = new Set(disksHistory.map((x) => x.name));
-    Array.from(distinctDisks).forEach((diskName) => {
-      const diskData = disksHistory.filter((x) => x.name === diskName);
-
-      const diskWithDiff = diskData.map(
-        (x) =>
-          <DiskDtoWithDiff>{
-            ...x,
-            usage_diff: 0,
-          }
-      );
-
-      diskWithDiff.forEach((entry, index) => {
-        if (index === 0) {
-          entry["usage_diff"] = 0;
-        } else {
-          const prevEntry = diskData[index - 1];
-          const diff =
-            ((prevEntry.available_space - entry.available_space) /
-              prevEntry.available_space) *
-            100;
-          entry["usage_diff"] = parseFloat(diff.toFixed(2));
-        }
-      });
-
-      const idDiskName = diskName.toLowerCase().replace(/\s+/g, "_");
-      const days = new Set(diskData.map((entry) => entry.date));
-
-      days.forEach((day) => {
-        const currentAgg = aggregatedData.find(
-          (entry) => entry.date.toISOString().split("T")[0] === day
-        );
-        const aggregatedEntry: any = currentAgg || { date: new Date(day) };
-        const entryForDay = diskWithDiff.find((entry) => entry.date === day);
-
-        if (entryForDay) {
-          aggregatedEntry[idDiskName] = entryForDay.usage_diff;
-        }
-
-        if (!currentAgg) {
-          aggregatedData.push(aggregatedEntry);
-        }
-      });
-
-      containerConfig[idDiskName] = {
-        label: diskName,
-        color: `var(--chart-${index})`,
-      };
-
-      chartConfig.push({
-        key: idDiskName,
-        label: diskName,
-        color: `var(--chart-${index++})`,
-      });
-    });
-  }
+  const {
+    availableSpaceReport,
+    usedSpaceChgPctReport,
+    usedSpacePctReport,
+    usedSpaceReport,
+  } = data;
 </script>
 
 <main class="container my-6">
   <h1 class="font-semibold text-lg mb-4">Charts</h1>
-
-  <Card.Root>
-    <Card.Header>
-      <Card.Title>Disk Usages Change in Percentage</Card.Title>
-      <Card.Description
-        >Showing a breakdown of individual disk usages as compared to yesterday</Card.Description
-      >
-    </Card.Header>
-    <Card.Content class="pl-12">
-      <Chart.Container config={containerConfig}>
-        <LineChart
-          data={aggregatedData}
-          x="date"
-          xScale={scaleUtc()}
-          series={chartConfig}
-          props={{
-            spline: { curve: curveNatural, motion: "tween", strokeWidth: 2 },
-            xAxis: {
-              format: (v: Date) =>
-                v.toLocaleDateString("en-US", { day: "2-digit" }),
-            },
-            yAxis: {
-              format: (v: number) => `${v.toFixed(1)}%`,
-              ticks: 5,
-            },
-            highlight: { points: { r: 4 } },
-          }}
-        >
-          {#snippet tooltip()}
-            <Chart.Tooltip hideLabel>
-              {#snippet formatter({ name, value, item })}
-                <div
-                  class="w-3 h-3 rounded"
-                  style={`background: ${item.color}`}
-                ></div>
-                <div
-                  class="text-muted-foreground flex min-w-[130px] items-center text-xs"
-                >
-                  {name}
-                  <div
-                    class="text-foreground ml-auto flex items-baseline gap-0.5 font-mono font-medium tabular-nums"
-                  >
-                    {value}
-                    <span class="text-muted-foreground font-normal"> % </span>
-                  </div>
-                </div>
-              {/snippet}
-            </Chart.Tooltip>
-          {/snippet}
-        </LineChart>
-      </Chart.Container>
-    </Card.Content>
-    <Card.Footer>
-      <div class="flex w-full items-start gap-2 text-sm">
-        <div class="grid gap-2">
-          <div class="flex items-center gap-2 font-medium leading-none">
-            Trending up by 5.2% this month <TrendingUpIcon class="size-4" />
-          </div>
-          <div
-            class="text-muted-foreground flex items-center gap-2 leading-none"
-          >
-            January - June 2024
-          </div>
-        </div>
-      </div>
-    </Card.Footer>
-  </Card.Root>
-
-  <div class="py-6"></div>
 
   <Card.Root>
     <Card.Header>
@@ -238,12 +96,12 @@
       >
     </Card.Header>
     <Card.Content class="pl-12">
-      <Chart.Container config={data.availableSpaceReport.containerConfig}>
+      <Chart.Container config={availableSpaceReport.containerConfig}>
         <LineChart
-          data={data.availableSpaceReport.data}
+          data={availableSpaceReport.data}
           x="date"
           xScale={scaleUtc()}
-          series={data.availableSpaceReport.chartConfig}
+          series={availableSpaceReport.chartConfig}
           props={{
             spline: { curve: curveNatural, motion: "tween", strokeWidth: 2 },
             xAxis: {
@@ -307,12 +165,12 @@
       >
     </Card.Header>
     <Card.Content class="pl-12">
-      <Chart.Container config={data.usedSpaceReport.containerConfig}>
+      <Chart.Container config={usedSpaceReport.containerConfig}>
         <LineChart
-          data={data.usedSpaceReport.data}
+          data={usedSpaceReport.data}
           x="date"
           xScale={scaleUtc()}
-          series={data.usedSpaceReport.chartConfig}
+          series={usedSpaceReport.chartConfig}
           props={{
             spline: { curve: curveNatural, motion: "tween", strokeWidth: 2 },
             xAxis: {
@@ -376,12 +234,12 @@
       >
     </Card.Header>
     <Card.Content class="pl-12">
-      <Chart.Container config={data.usedSpacePctReport.containerConfig}>
+      <Chart.Container config={usedSpacePctReport.containerConfig}>
         <LineChart
-          data={data.usedSpacePctReport.data}
+          data={usedSpacePctReport.data}
           x="date"
           xScale={scaleUtc()}
-          series={data.usedSpacePctReport.chartConfig}
+          series={usedSpacePctReport.chartConfig}
           props={{
             spline: { curve: curveNatural, motion: "tween", strokeWidth: 2 },
             xAxis: {
