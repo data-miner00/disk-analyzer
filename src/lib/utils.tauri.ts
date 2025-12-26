74ca9bca1b9e5445b926bc0a794aefe1bc27dff4
Shaun Chong <data-miner00@users.noreply.github.com>
2025-12-26 19:30:21 +0800

    fix: Return null instead of default object when no history found

--- src/lib/utils.tauri.ts
+++ src/lib/utils.tauri.ts
@@ -7,18 +7,13 @@ export async function getSettings(): Promise<Settings> {
   return convertObjectKeysToCamelCase(setting) as Settings;
 }
 
-export async function getLastHistoryEntry(): Promise<DiskHistory> {
+export async function getLastHistoryEntry(): Promise<DiskHistory | null> {
   const diskHistory = (await invoke("read_disk_dtos", {
     count: 1,
   })) as DiskHistory[];
 
   if (diskHistory.length === 0) {
-    return {
-      id: 0,
-      name: "",
-      date: new Date().toISOString(),
-      available_space: 0,
-    };
+    return null;
   }
 
   return diskHistory[0];
